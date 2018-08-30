f2397153ea68443abbb8f8f448d7ae321746c865
Kitetop <1363215999@qq.com>
2018-08-30 15:59:19 +0800

    更改了虚拟机创建所需要执行的脚本命令，创建 PHP + Apeach 开发环境；更新了Vagrantfile-simple文件内容，用于适应以后要同时创建多台虚拟机的情况

---
+++ environment_dev.sh
@@ -0,0 +1,27 @@
+echo "开始初始化:"
+# php7.2 安装
+  # 移除早前版本的 php
+  yum -y remove php*
+  # 更新 yum 源
+  rpm -Uvh http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm   
+  rpm -Uvh http://mirror.webtatic.com/yum/el7/webtatic-release.rpm  
+  # php 版本安装以及常用扩展安装
+  yum -y install php72w php72w-cli php72w-common php72w-devel php72w-pear php72w-embeded php72w-fpm
+  
+# Apeach 的安装
+  yum -y install httpd
+
+# 切换管理员权限
+  sudo su
+# Composer 的全局安装
+  curl -sS https://getcomposer.org/installer | php
+  mv composer.phar /usr/local/bin/composer
+ 
+# 重新启动 Apeach
+  service httpd restart
+
+# vim 安装
+  yum -y install vim
+
+# tmux 安装
+  yum -y install tmux
