8ff3f5afec88b4ffaa928eed228495c88a655db9
Kitetop <1363215999@qq.com>
2018-08-31 10:26:06 +0800

    update environmend_dev.sh

--- environment_dev.sh
+++ environment_dev.sh
@@ -2,9 +2,11 @@ echo "开始初始化:"
 # php7.2 安装
   # 移除早前版本的 php
   yum -y remove php*
+
   # 更新 yum 源
   rpm -Uvh http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm   
-  rpm -Uvh http://mirror.webtatic.com/yum/el7/webtatic-release.rpm  
+  rpm -Uvh http://mirror.webtatic.com/yum/el7/webtatic-release.rpm
+
   # php 版本安装以及常用扩展安装
   yum -y install php72w php72w-cli php72w-common php72w-devel php72w-pear php72w-embeded php72w-fpm
   
@@ -13,6 +15,7 @@ echo "开始初始化:"
 
 # 切换管理员权限
   sudo su
+
 # Composer 的全局安装
   curl -sS https://getcomposer.org/installer | php
   mv composer.phar /usr/local/bin/composer
